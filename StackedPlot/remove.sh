b6e60f3a62ddb7f0e20a44e3279548da9c5cb70f
허 창기 <cghuh3811@changgihuh.dhcp.fnal.gov>
2017-09-26 11:31:03 -0500

    Stacked plot codes

---
+++ StackedPlot/remove.sh
@@ -0,0 +1,2 @@
+echo "Warning!! You remove the Histograms"
+rm -rf Histogram/2D/*;rm -rf Histogram/1D;rm -f Histogram/*png
---
+++ StackedPlot/run.sh
@@ -0,0 +1,4 @@
+source remove.sh;
+./StackedPlot;
+#./StackedPlot_2D;
+source mvfile.sh;
